6ea320ed3d85144410960f52bf68ac083113d585
Cam <webdevbycam@gmail.com>
2025-03-31 16:05:54 -0500

    juan

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -22,7 +22,7 @@ function LoginForm({
                         name="loginId"
                         value={formData.loginId}
                         onChange={handleChange}
-                        placeholder="Camilo"
+                        placeholder="Juan Andres"
                         required
                         aria-describedby="loginId-error"
                     />
