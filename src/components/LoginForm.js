6ce48f1e1397609b668766cb7a7963de1b40e6da
Cam <webdevbycam@gmail.com>
2025-04-01 15:22:13 -0500

    push

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -22,7 +22,7 @@ function LoginForm({
                         name="loginId"
                         value={formData.loginId}
                         onChange={handleChange}
-                        placeholder="Juan Camilo de las Nieves"
+                        placeholder="Diego Armando Maradona"
                         required
                         aria-describedby="loginId-error"
                     />
