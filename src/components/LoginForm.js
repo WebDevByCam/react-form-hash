679624d5f0f82e15e8ed3f000a0e0b25abdaccd1
Cam <webdevbycam@gmail.com>
2025-04-01 16:43:59 -0500

    edits

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -22,7 +22,7 @@ function LoginForm({
                         name="loginId"
                         value={formData.loginId}
                         onChange={handleChange}
-                        placeholder="Lionel Messi"
+                        placeholder="El Tigre Falcao"
                         required
                         aria-describedby="loginId-error"
                     />
